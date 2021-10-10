4d293be78a946804ba8e5f3c51b747b370485281
Lior3796 <liorlsa9@gmail.com>
2021-10-10 16:06:02 +0300

    added server boilerplate

--- server/server.js
+++ server/server.js
@@ -7,19 +7,19 @@ const cors = require("cors");
 app.use(cors());
 
 const db = require("./DB");
-const StudentRouter = require("./Routers/StudentRouter");
+
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.send("we up");
+app.get("/", (req, res) => {
+    res.send("Server is up");
 })
 
-app.use("/student",StudentRouter)
 
-app.listen(PORT,(err,result)=>{
-    if(err) throw err;
+
+app.listen(PORT, (err, result) => {
+    if (err) throw err;
     console.log("server up");
 })
 
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '../client/build')));
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res)=>{
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
     });
 }
