5d767e243d32ddbb6df753ef415c7e512a71864b
sigalavera <sigi1697@gmail.com>
2021-10-11 23:24:22 +0300

    navbar

--- client/src/Components/Features/Header/HeaderComponent.jsx
+++ client/src/Components/Features/Header/HeaderComponent.jsx
@@ -1,31 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Login from "../../Pages/Login/LoginComponent";
+import Navbar from '../Navbar/NavbarComponent';
 
 const Header = () => {
   return (
     <div>
       HEADER
-      <nav>
-        <div>
-          <Login/>
-        </div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/chat">Chat</Link>
-          </li>
-          <li>
-            <Link to="/InterviewQuestions">Interview Questions</Link>
-          </li>
-          <li>
-            <Link to="/StudyContent">Study Content</Link>
-          </li>
-         
-        </ul>
-      </nav>
+      <Navbar/>
     </div>
   );
 };
