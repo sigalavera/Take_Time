3f019dcad989167884613d4d40729de42b1831f9
sigalavera <sigi1697@gmail.com>
2021-10-11 04:50:08 +0300

    Modeling

---
+++ client/src/Components/Features/AddQuestion/AddQuestionComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddQuestion = () => {
+    return (
+        <>
+            <h1>
+                Add question
+            </h1>
+        </>
+    )
+}
+export default AddQuestion;
---
+++ client/src/Components/Pages/StudyContent/StudyContentComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const StudyContent = () => {
+    return (
+        <h1>
+            Study content
+        </h1>
+    )
+}
+export default StudyContent;
