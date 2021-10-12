c9b60fbb819c395ebc4859c5293563760ed03afc
SlumpG <shlomo.gangul1@gmail.com>
2021-10-12 21:45:27 +0300

    basic profile

--- client/src/Components/Pages/Login/LoginComponent.jsx
+++ client/src/Components/Pages/Login/LoginComponent.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
-import NavBar from "../../Features/Navbar/NavbarComponent";
 import "./login.css";
 import { AUTH } from "../../../Redux/Actions/types";
 
@@ -22,29 +20,8 @@ const Login = () => {
     try {
       dispatch({ type: AUTH, data: { result, token } });
       console.log(res);
-      let data = `{
-   "username": "${res.profileObj.name}",
-   "secret": "${res.profileObj.googleId}",
-   "email": "${res.profileObj.email}",
-   "first_name": "${res.profileObj.givenName}",
-   "last_name": "${res.profileObj.familyName}"
-     }`;
-     console.log(data);
-      let config = {
-        method: "post",
-        url: "https://api.chatengine.io/users/",
-        headers: "PRIVATE-KEY : f6095433-da83-4fba-b6d7-cf422d6293b5",
-        data: data,
-      };
-
-      await axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-      history.push("/");
+  
+      history.push("/home");
     } catch (error) {
       console.log(error);
     }
