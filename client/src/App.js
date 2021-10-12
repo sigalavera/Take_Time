a3e5cc1b4c058d04f0aa6b96845c333973a9a177
Lior3796 <liorlsa9@gmail.com>
2021-10-12 10:24:01 +0300

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AppRouter from "./Components/Features/AppRouter/AppRouterComponent";
+
 
 function App() {
   return (
