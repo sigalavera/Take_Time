55122ec1d78cc56809e6b96a2df51bc1ef58dccc
Lior3796 <liorlsa9@gmail.com>
2021-10-13 07:45:43 +0300

    add router and controller to user

--- client/src/Components/Features/Header/HeaderComponent.jsx
+++ client/src/Components/Features/Header/HeaderComponent.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import NavBar from "../Navbar/NavbarComponent";
 import "./header.css";
 
 const Header = () => {
+  const [userName,setUserName] = useState();
+  const getUserName = () => {
+   const {result} = localStorage.getItem("userProfile");
+   console.log(JSON.parse(result));
+  }
+  useEffect(()=>{
+    getUserName();
+  })
   return (
     <div className="continer">
       <NavBar/>
       <div>
-        <h1>Take Time .</h1>
+        <h1>HEY</h1>
         </div>
     </div>
   );
