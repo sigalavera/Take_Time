48640cc750d00f55df310c82f0811fa382072feb
Lior3796 <liorlsa9@gmail.com>
2021-10-13 07:01:22 +0300

    fix chat local storage

--- client/src/Components/Features/AddQuestion/AddQuestionComponent.jsx
+++ client/src/Components/Features/AddQuestion/AddQuestionComponent.jsx
@@ -7,22 +7,22 @@ const AddQuestion = () => {
         createdAt: ""
     })
 
-    fetch("http://localhost:5000/question/getAlllQuestions")
-    handleChange = (e) => {
-        setNewQuestion(
-            {
-                ...newQuestion, 
-                [e.target.name]: e.target.value
-            }
-        )
-    }
+    // fetch("http://localhost:5000/question/getAlllQuestions")
+    // handleChange = (e) => {
+    //     setNewQuestion(
+    //         {
+    //             ...newQuestion, 
+    //             [e.target.name]: e.target.value
+    //         }
+    //     )
+    // }
     return (
         <div>
             <h1> דוגמא לשאלה </h1>
 
-            <textarea name="question" cols="30" rows="2" placeholder="הקלד שאלה" onChange={(e)=>{handleChange(e)}}></textarea>
+            {/* <textarea name="question" cols="30" rows="2" placeholder="הקלד שאלה" onChange={(e)=>{handleChange(e)}}></textarea> */}
             <br></br>
-            <textarea name="answer" cols="30" rows="2" placeholder="הקלד תשובה" onChange={(e)=>{handleChange(e)}}></textarea>
+            {/* <textarea name="answer" cols="30" rows="2" placeholder="הקלד תשובה" onChange={(e)=>{handleChange(e)}}></textarea> */}
             <br></br>
             <button type="submit"> שלח </button>
         </div>
--- client/src/Components/Pages/Chat/DirectChat/DM.jsx
+++ client/src/Components/Pages/Chat/DirectChat/DM.jsx
@@ -1,15 +1,14 @@
-
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
 import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
 
-const DirectChatPage = () => {
-	const [username, setUsername] = useState('')
-
+const DM = (props) => {
+	const [username, setUsername] = useState('');
+    
+    console.log(props);
 	function createDirectChat(creds) {
 		getOrCreateChat(
 			creds,
-			{ is_direct_chat: true, usernames: [username] },
+			{ is_direct_chat: true, usernames: props.userName },
 			() => setUsername('')
 		)
 	}
@@ -19,7 +18,7 @@ const DirectChatPage = () => {
 			<div>
 				<input 
 					placeholder='Username' 
-					value="liorlsa9" 
+					value='Sigal1' 
 					onChange={(e) => setUsername(e.target.value)} 
 				/>
 				<button onClick={() => createDirectChat(creds)}>
@@ -31,9 +30,8 @@ const DirectChatPage = () => {
 
 	return (
 		<ChatEngine
-            userName="dsa"
 			height='100vh'
-			userName='Liorlsa9'
+			userName={username}
 			userSecret='Liorlsa9'
 			projectID='654ce782-8e95-433a-b3d6-11e63df52204'
 			renderNewChatForm={(creds) => renderChatForm(creds)}
@@ -41,4 +39,4 @@ const DirectChatPage = () => {
 	)
 }
 
-export default DirectChatPage;
+export default DM;
